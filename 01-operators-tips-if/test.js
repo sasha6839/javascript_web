b71e67507551d2813ea862b9182235735aa367b5
sasha6839 <saskohrapan@gmail.com>
2025-03-01 12:04:12 +0200

    les 1

---
+++ 01-operators-tips-if/test.js
@@ -0,0 +1,17 @@
+// коментує цей рядок
+
+
+/*
+коментар
+на
+декілька
+рядків
+*/
+
+let myVariable = 12;
+let number = 3.33;
+let text = "Text";
+
+const MY_NAME = "Sanya"
+
+myVariable = number + myVariable
