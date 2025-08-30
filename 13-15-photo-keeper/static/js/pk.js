0d13b69bde8032bf73b7945c90ab34a1001b8a55
david <barnakdavid49@gmail.com>
2025-08-30 12:01:15 +0300

    gfsdg

--- 13-15-photo-keeper/static/js/pk.js
+++ 13-15-photo-keeper/static/js/pk.js
@@ -2,5 +2,36 @@ console.log('start');
 
 
 class MainMenu {
-    
+    constructor() {
+        this.numbers = [];
+    }
+
+    addNumber(num) {
+        if (typeof num === 'number') {
+            this.numbers.push(num);
+        }
+    }
+
+    removeNumber(num) {
+        this.numbers = this.numbers.filter(n => n !== num);
+    }
+
+    countTagCharacters(tagName) {
+        const element = document.querySelector(tagName);
+        if (element && element.textContent) {
+            return element.textContent.length;
+        }
+        return 0;
+    }
+    multiplyNumbers(a, b) {
+       return a * b;
+    }
+    divideNumbers(a, b) {
+       if (b === 0) {
+           return null; // Avoid division by zero
+       }
+       return a / b;
+    }   
+
+
 }
